dc48a69d4b3e682d57e596d2f9b829de32c70717
PineGolden311 <software.developer112@gmail.com>
2019-04-16 20:19:22 -0600

    implement youtube channel search feature

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React, { Component } from 'react';
 import { withAuthenticator } from 'aws-amplify-react'
-import './App.css';
+import Youtube from './components/youtube/index'
+import './App.scss';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <h1>This is an example of fetching Youtube Channel</h1>
-      </div>
-    );
+    return <Youtube />
   }
 }
 
